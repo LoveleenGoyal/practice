248c0ef68580dea62afa06faf2448f4bf3557f5c
LoveleenGoyal <loveleengoyal26@gmail.com>
2025-02-25 14:46:32 +0530

    Loveleen MOD:modified the file

--- HelloLoveleen.java
+++ HelloLoveleen.java
@@ -1,5 +1,5 @@
 public class HelloLoveleen{
 	public static void main(String[] args){
-		System.out.println("Hello,My name is Loveleen Goyal");
+		System.out.println("Hello,My name is Yagyata..");
 	}
 }
